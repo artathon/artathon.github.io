fad8f770166f91a03712b7a8828b99f13764b467
griffincorriher <griffincorriher@gmail.com>
2022-12-18 03:16:20 -0500

    Updated image locations

--- src/data/devData.js
+++ src/data/devData.js
@@ -2,14 +2,14 @@ export default [
     {
         name: 'Griffin Corriher',
         degree: 'Data Science and Business Analytics',
-        imgUrl: './src/assets/griffin.jpg',
+        imgUrl: './dist/griffin.jpg',
         link: 'https://www.linkedin.com/in/griffincorriher',
         gradDate: 'May 2024'
     },
     {
         name: 'Pruthvi Raj',
         degree: 'Data Science and Business Analytics',
-        imgUrl: './src/assets/pruthvi.jpg',
+        imgUrl: './dist/pruthvi.jpg',
         link: 'https://www.linkedin.com/in/pruthvi-raj-228816161/',
         gradDate: 'May 2024'
     },
